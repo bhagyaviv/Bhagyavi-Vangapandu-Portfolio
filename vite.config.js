a248e4919eb3954526286bce4e990ecb45ce3f7c
arimaakousei <bhagyaviv@gmail.com>
2026-07-02 11:04:08 +0530

    Config: Setup deploy paths and gh-pages scripts

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Bhagyavi-Vangapandu-Portfolio/",
   plugins: [
     react(),
     tailwindcss()
